08602e54d9961768a5dd58be5a215260800f1025
Sanya <oleksandr.v.radchenko@ukd.edu.ua>
2024-04-11 22:07:10 +0300

    nu tut uzhe vsyo

--- s1te/main/views.py
+++ s1te/main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Main page',
+        'values': ['some', 'shishki', 'nasvay'],
+        'obj': {
+            'car': 'RX7',
+            'grams': 500,
+                     'money': '9999999BTC',
+    }   }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
--- s1te/s1te/settings.py
+++ s1te/s1te/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'news',
     'main',
     'django.contrib.admin',
     'django.contrib.auth',
